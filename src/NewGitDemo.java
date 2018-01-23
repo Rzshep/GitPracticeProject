830ad8ce1b1c0d933a61e70e372bda123f615595
Rzshep <ronaldjiwonkim@gmail.com>
2018-01-23 14:34:14 -0500

    GitPractice Commit

---
+++ src/NewGitDemo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter your first name: ");
+		String firstName = scan.nextLine();
+		
+		System.out.println(firstName);
+
+	}
+
+}
